86dccb658f5c59d409c9e88b9dba0d797047288b
John Potts <john@arlingtonchurch.com>
2017-08-15 05:53:49 -0400

    Updated keyboard functonality

--- calculator/script.js
+++ calculator/script.js
@@ -28,27 +28,7 @@ value="",plusValue=0, minusValue=0,dividedValue=0,timesValue=0;
 
 $('.btn-fx').hover(function(){$(this).toggleClass('button_hover');});
 
-posNegButton.onclick=function(){if (value.charAt(0)=="-"){
-  value=value.slice(1,value.length)
-}
-else{value="-"+value}; updateDisplay();}
-percentButton.onclick=function(){value = (Number(value)/100).toString(); updateDisplay();}
-squareButton.onclick=function(){value = Math.pow(Number(value),2).toString(); updateDisplay();}
-reciprocalButton.onclick=function(){value = (1/Number(value)).toString(); updateDisplay();}
-sqrtButton.onclick=function(){value=Math.sqrt(Number(value)).toString(); updateDisplay();}
-deleteButton.onclick=function(){ value= value.substring(0,value.length-1); updateDisplay();}
-oneButton.onclick=function(){value +="1"; updateDisplay();}
-twoButton.onclick=function(){value +="2"; updateDisplay();}
-threeButton.onclick=function(){value +="3"; updateDisplay();}
-fourButton.onclick=function(){value +="4"; updateDisplay();}
-fiveButton.onclick=function(){value +="5"; updateDisplay();}
-sixButton.onclick=function(){value +="6"; updateDisplay();}
-sevenButton.onclick=function(){value +="7"; updateDisplay();}
-eightButton.onclick=function(){value +="8"; updateDisplay();}
-nineButton.onclick=function(){value +="9"; updateDisplay();}
-zeroButton.onclick=function(){value +="0"; updateDisplay();}
-
-plusButton.onclick=function(){
+var plusFunction = function(){
   if (plusValue!=0){
       plusValue= Number(value)+plusValue;
       value="";
@@ -76,7 +56,8 @@ timesValue=0;
          {plusValue=Number(value); value=""; updateDisplay();}
        }
 
-minusButton.onclick=function(){
+
+var minusFunction = function(){
   if (plusValue!=0){
       minusValue= Number(value)+plusValue;
       value="";
@@ -103,34 +84,37 @@ timesValue=0;
          else
          {minusValue=Number(value); value=""; updateDisplay();}
        }
-dividedButton.onclick=function(){
-  if (plusValue!=0){
-      dividedValue= Number(value)+plusValue;
-      value="";
-        plusValue=0;
-      updateDisplay();
-  }
-  else if(minusValue!=0){
-    dividedValue=minusValue-Number(value);
-      value= "";
-      minusValue=0;
-        updateDisplay();
-    }
-    else if(dividedValue!=0){
-      dividedValue= dividedValue/Number(value);
-value="";
-          updateDisplay();
-    }
-      else if(timesValue!=0){
-        dividedValue= timesValue*Number(value);
-value="";
-timesValue=0;
-            updateDisplay();
+
+var dividedFunction =function(){
+         if (plusValue!=0){
+             dividedValue= Number(value)+plusValue;
+             value="";
+               plusValue=0;
+             updateDisplay();
          }
-         else
-         {dividedValue=Number(value); value=""; updateDisplay();}
-       }
-timesButton.onclick=function(){
+         else if(minusValue!=0){
+           dividedValue=minusValue-Number(value);
+             value= "";
+             minusValue=0;
+               updateDisplay();
+           }
+           else if(dividedValue!=0){
+             dividedValue= dividedValue/Number(value);
+       value="";
+                 updateDisplay();
+           }
+             else if(timesValue!=0){
+               dividedValue= timesValue*Number(value);
+       value="";
+       timesValue=0;
+                   updateDisplay();
+                }
+                else
+                {dividedValue=Number(value); value=""; updateDisplay();}
+              }
+
+var timesFunction = function(){
+
   if (plusValue!=0){
       timesValue= Number(value)+plusValue;
       plusValue=0;
@@ -157,32 +141,75 @@ value="";
          else
          {timesValue=Number(value); value=""; updateDisplay();}
        }
-equalsButton.onclick=function(){
-  if (plusValue!=0){
-      value= (Number(value)+plusValue).toString();
-      updateDisplay();
-    plusValue=0;
-  }
-  else if(minusValue!=0){
-    value= (minusValue-Number(value)).toString();
-        updateDisplay();
-    minusValue=0;  }
-    else if(dividedValue!=0){
-      value= (dividedValue/Number(value)).toString();
-          updateDisplay();
-    dividedValue=0;  }
-      else if(timesValue!=0){
-        value= (timesValue*Number(value)).toString();
-            updateDisplay();
-        timesValue=0;  }
+var equalsFunction = function(){
+              if (plusValue!=0){
+                  value= (Number(value)+plusValue).toString();
+                  updateDisplay();
+                plusValue=0;
+              }
+              else if(minusValue!=0){
+                value= (minusValue-Number(value)).toString();
+                    updateDisplay();
+                minusValue=0;  }
+                else if(dividedValue!=0){
+                  value= (dividedValue/Number(value)).toString();
+                      updateDisplay();
+                dividedValue=0;  }
+                  else if(timesValue!=0){
+                    value= (timesValue*Number(value)).toString();
+                        updateDisplay();
+                    timesValue=0;  }
+            }
+var posNegFunction= function(){if (value.charAt(0)=="-"){
+  value=value.slice(1,value.length)
 }
-clearButton.onclick=function(){
+else{value="-"+value}; updateDisplay();}
+
+var clearFunction = function(){
   value=""; plusValue=0; minusValue=0;dividedValue=0;timesValue=0;updateDisplay();
 }
-clearEntryButton.onclick=function(){
+var clearEntryFunction= function(){
   value=""; updateDisplay();
-}
-decimalButton.onclick=function(){value +="."; updateDisplay();}
+};
+
+var delFunction= function delFunction(){ value= value.substring(0,value.length-1); updateDisplay();};
+
+var squareFunction = function(){value = Math.pow(Number(value),2).toString(); updateDisplay();};
+
+var sqrtFunction = function(){value=Math.sqrt(Number(value)).toString(); updateDisplay();};
+
+var reciprocalFunction = function(){value = (1/Number(value)).toString(); updateDisplay();};
+
+var percentFunction = function(){value = (Number(value)/100).toString(); updateDisplay();};
+
+posNegButton.onclick= posNegFunction();
+percentButton.onclick=percentFunction();
+squareButton.onclick=squareFunction();
+reciprocalButton.onclick=reciprocalFunction();
+sqrtButton.onclick=sqrtFunction();
+deleteButton.onclick=delFunction()
+oneButton.onclick=function(){value +="1"; updateDisplay();}
+twoButton.onclick=function(){value +="2"; updateDisplay();}
+threeButton.onclick=function(){value +="3"; updateDisplay();}
+fourButton.onclick=function(){value +="4"; updateDisplay();}
+fiveButton.onclick=function(){value +="5"; updateDisplay();}
+sixButton.onclick=function(){value +="6"; updateDisplay();}
+sevenButton.onclick=function(){value +="7"; updateDisplay();}
+eightButton.onclick=function(){value +="8"; updateDisplay();}
+nineButton.onclick=function(){value +="9"; updateDisplay();}
+zeroButton.onclick=function(){value +="0"; updateDisplay();}
+plusButton.onclick= plusFunction();
+minusButton.onclick=minusFunction();
+dividedButton.onclick= dividedFunction();
+timesButton.onclick=timesFunction();
+equalsButton.onclick=equalsFunction();
+clearButton.onclick=clearFunction();
+clearEntryButton.onclick=clearEntryFunction();
+decimalButton.onclick=function(){value +="."; updateDisplay();};
+
+
+
+
 
 function updateDisplay(){
   if(value){
@@ -198,5 +225,104 @@ function updateDisplay(){
   else if (timesValue!=0){display.innerHTML=(timesValue);}
   else{display.innerHTML="0";}
 }
+
+$(document).keydown(function(event){
+  switch(event.which){
+    case 48:
+    case 96:
+    value +="0"; updateDisplay();
+    break;
+    case 49:
+    case 97:
+    value +="1"; updateDisplay();
+    break;
+    case 50:
+    case 98:
+    value +="2"; updateDisplay();
+    break;
+    case 51:
+    case 99:
+    value +="3"; updateDisplay();
+    break;
+    case 52:
+    case 100:
+    value +="4"; updateDisplay();
+    break;
+    case 53:
+    case 101:
+    value +="5"; updateDisplay();
+    break;
+    case 54:
+    case 102:
+    value +="6"; updateDisplay();
+    break;
+    case 55:
+    case 103:
+    value +="7"; updateDisplay();
+    break;
+    case 56:
+    case 104:
+    value +="8"; updateDisplay();
+    break;
+    case 57:
+    case 105:
+    value +="9"; updateDisplay();
+    break;
+    case 13:
+    case 187:
+    equalsFunction();
+    break;
+    case 110:
+    case 190:
+    value+="."; updateDisplay();
+    break;
+    case 106:
+    case 88:
+    timesFunction();
+    break;
+    case 107:
+    case 80:
+    plusFunction();
+    break;
+    case 109:
+    case 189:
+    case 77:
+    minusFunction();
+    break;
+    case 111:
+    case 191:
+    case 68:
+    dividedFunction();
+    break;
+    case 8:
+    case 46:
+    delFunction();
+    break;
+    case 9:
+    posNegFunction();
+    break;
+    case 70:
+    percentFunction();
+    break;
+    case 67:
+    clearFunction();
+    break;
+    case 69:
+    clearEntryFunction();
+    break;
+    case 81:
+    sqrtFunction();
+    break;
+    case 83:
+    squareFunction();
+    break;
+    case 82:
+    reciprocalFunction();
+    break;
+    default:
+    alert(event.which);
+    break;
+  }
+});
 
 });
